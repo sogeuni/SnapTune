afe92faf48ac75a9b254af4e42c84254eff9f1a8
[OBIGO] Darren Park / 박석원 <darren.park@obigo.com>
2026-02-06 16:12:51 +0900

    Modernize CI/CD pipeline and optimize build performance

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,43 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     id("org.sonarqube")
+    id("jacoco")
+}
+
+allprojects {
+    apply(plugin = "jacoco")
+}
+
+subprojects {
+    tasks.register<JacocoReport>("jacocoTestReport") {
+        val testTaskName = if (plugins.hasPlugin("com.android.application") || plugins.hasPlugin("com.android.library")) {
+            "testDebugUnitTest"
+        } else {
+            "test"
+        }
+
+        tasks.findByName(testTaskName)?.let { dependsOn(it) }
+
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+
+        val fileFilter = listOf(
+            "**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*",
+            "**/*Test*.*", "android/**/*.*", "**/databinding/**/*.*", "**/BR.class"
+        )
+        val debugTree = fileTree("${project.buildDir}/intermediates/javac/debug/classes") {
+            exclude(fileFilter)
+        }
+        val mainSrc = "${project.projectDir}/src/main/java"
+
+        sourceDirectories.setFrom(files(mainSrc))
+        classDirectories.setFrom(files(debugTree))
+        executionData.setFrom(fileTree(project.buildDir) {
+            include("jacoco/testDebugUnitTest.exec", "jacoco/test.exec")
+        })
+    }
 }
 
 sonar {
@@ -9,5 +46,12 @@ sonar {
         property("sonar.projectKey", "sogeuni_SnapTune")
         property("sonar.organization", "sogeuni")
         property("sonar.host.url", "https://sonarcloud.io")
+
+        // Gradle property(gradle.properties) or Environment Variable
+        (findProperty("sonar.token") ?: System.getenv("SONAR_TOKEN"))?.let {
+            property("sonar.token", it)
+        }
+
+        property("sonar.coverage.jacoco.xmlReportPaths", "${project.projectDir}/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
     }
 }
---
+++ shared/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.sogeuni.snaptune.shared"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.media)
+}
