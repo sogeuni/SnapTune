4165c93784a1f941d630f109db5556939aa4f80d
[OBIGO] Darren Park / 박석원 <darren.park@obigo.com>
2026-02-10 15:34:00 +0900

    android lint

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -29,6 +29,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    lint {
+        sarifReport = true
+        abortOnError = false
+    }
 }
 
 dependencies {
